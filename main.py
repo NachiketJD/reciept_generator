12dade8251586cfb0e7a8e856fff7b0fb8690692
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-04 15:35:00 +0530

    4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle 
 from reportlab.lib.pagesizes import A4 
 from reportlab.lib import colors 
+from reportlab.lib.styles import getSampleStyleSheet
