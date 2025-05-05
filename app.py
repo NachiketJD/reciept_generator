a46dd85af3cc31fbf589e3f82b02746f9ff83969
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-05 19:38:30 +0530

    24

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask, request, send_file, render_template
+from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+import tempfile
